683d07f699ceb69dd50a60e0f453b29f74de696b
TunkShif <tunkshif@foxmail.com>
2023-04-19 20:53:57 +0800

    upadte/common: popup content tabs

--- packages/common/src/components/reader/popup/content.tsx
+++ packages/common/src/components/reader/popup/content.tsx
@@ -3,9 +3,24 @@ import { IconButton } from "@/components/ui"
 import { content } from "@/components/ui/styles"
 import { Tabs as KTabs } from "@kobalte/core"
 import { IconEdit } from "@tabler/icons-solidjs"
+import { onMount } from "solid-js"
 import { twMerge } from "tailwind-merge"
 
 // TODO: long selected text styling
+
+const Foo = () => {
+  onMount(() => {
+    console.log("Foo mounted")
+  })
+  return <div>foo</div>
+}
+
+const Bar = () => {
+  onMount(() => {
+    console.log("Bar mounted")
+  })
+  return <div>bar</div>
+}
 
 const Content = () => {
   const context = usePopupContext()
@@ -25,30 +40,36 @@ const Content = () => {
           </div>
 
           <div>
-            <p class="text-justify text-sm leading-tight">{context()?.context}</p>
+            <p class="text-sm leading-tight">{context()?.context}</p>
           </div>
         </div>
       </div>
 
       <div class={twMerge(content, "w-96")}>
         <KTabs.Root class="w-full">
-          <KTabs.List class="flex h-8 w-full items-center space-x-2 rounded bg-slate-100 p-1">
+          <KTabs.List class="inline-flex h-8 items-center space-x-2 rounded-sm bg-slate-100 p-1 shadow-sm dark:bg-slate-700 dark:shadow-none dark:ring-1 dark:ring-inset dark:ring-white/10">
             <KTabs.Trigger
               value="foo"
-              class="ui-selected:text-slate-900 ui-selected:bg-white h-6 w-6 rounded p-0.5 text-xs"
+              class="ui-selected:text-slate-900 ui-selected:bg-white dark:ui-selected:bg-slate-200 h-6 rounded-sm px-1.5 text-xs"
             >
-              Fo
+              SpanishDict
             </KTabs.Trigger>
             <KTabs.Trigger
               value="bar"
-              class="ui-selected:text-slate-900 ui-selected:bg-white h-6 w-6 rounded p-0.5 text-xs"
+              class="ui-selected:text-slate-900 ui-selected:bg-white dark:ui-selected:bg-slate-200 h-6 rounded-sm px-1.5 text-xs"
             >
-              Ba
+              Bar
             </KTabs.Trigger>
           </KTabs.List>
 
-          <KTabs.Content value="foo">Foo Content</KTabs.Content>
-          <KTabs.Content value="bar">Bar Content</KTabs.Content>
+          <div class="p-2 pt-4">
+            <KTabs.Content value="foo">
+              <Foo />
+            </KTabs.Content>
+            <KTabs.Content value="bar">
+              <Bar />
+            </KTabs.Content>
+          </div>
         </KTabs.Root>
       </div>
     </div>
